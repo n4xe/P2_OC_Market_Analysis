04a854d266e9d27977053fa74e6c224d0f0c599e
Valentin Simioni <valentin.simioni@outlook.com>
2023-03-24 10:34:20 +0100

    last fix (root repository created)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ input("\n \n !Press enter to continue and scrap book data!")
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-STEP 1 : COLLECT CATEGORY NAMES AND LINKS THROUGH INDEX PAGE
+STEP 1 : COLLECT CATEGORY NAMES AND LINKS THROUGH INDEX PAGE (ETL CATEGORIES)
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 url_index_page = "http://books.toscrape.com/catalogue/category/books_1/index.html"  # Web page URL
@@ -22,7 +22,7 @@ index_page = requests.get(url_index_page)  # Use of Request
 soup = BeautifulSoup(index_page.content, 'html.parser')  # Use of BS4 to parser
 
 """
-1.a Collect raw category names and links
+1.a Collect raw category names and links (E)
 """
 
 dirty_category_names = []  # List of uncleaned category names
@@ -40,7 +40,7 @@ for links in link_tags:
 
 
 """
-1.b Cleaning of raw category names and links
+1.b Cleaning of raw category names and links (T)
 """
 
 # Cleaning of raw category names and links with the use of function created
@@ -50,7 +50,7 @@ to_remove_names = {" ": "", "\n": ""}  # dictionary for names. Keys : to remove,
 cleaned_links = clinks(list_to_clean=dirty_category_links, to_remove=to_remove_links)
 cleaned_names = clinks(list_to_clean=dirty_category_names, to_remove=to_remove_names)
 
-# Creation of names/links dictionary with cleaned data
+# Creation of names/links dictionary with cleaned data (L)
 category_links_dictionary = {"Names": cleaned_names, "Links": cleaned_links}
 
 # print(category_links_dictionary)
@@ -62,8 +62,8 @@ STEP 2 : SCAN OF EACH CATEGORY LINKS BY APPLYING A SCRAPING LOOP
 2.a Declaration of required elements + "for" loop creation (category range iteration, == 40)
 """
 
-liste_elements = ["product_page_urls", "title", "upc", "prices_inc_tax", "prices_ex_tax", "nb_available", "rating",
-                  "product description", "image", "category"]
+elements_list = ["product_page_urls", "title", "upc", "prices_inc_tax", "prices_ex_tax", "nb_available", "rating",
+                 "product description", "image", "category"]
 
 book_qty = soup.find("form", class_="form-horizontal")  # book quantity to display scraping achievement
 qty_string = book_qty.find("strong").text
@@ -73,7 +73,7 @@ qty_scraped = 0
 for category in (range(len(cleaned_links))):
 
     # First ULR == 1st dict value == "travel" category link. Will take next one after the end of the loop.
-    url_actuel = cleaned_links[category]
+    actual_url = cleaned_links[category]
     actual_category = cleaned_names[category]
     print("\n" + actual_category + " category is being scraped")
 
@@ -81,7 +81,12 @@ for category in (range(len(cleaned_links))):
     2.b Creation of the data saving folder 
     """
 
-    path = "{} folder extract".format(cleaned_names[category])
+    main_repository = "Extractions"
+    isExist_main = os.path.exists(main_repository)
+    if not isExist_main:
+        os.makedirs(main_repository)
+
+    path = "Extractions/ {} folder extract".format(cleaned_names[category])
     # Check whether the specified path exists or not
     isExist = os.path.exists(path)
     if not isExist:
@@ -97,7 +102,7 @@ for category in (range(len(cleaned_links))):
 
     with open(path_file, 'w') as csv_file:  # Creation of the csv with the required elements as headers
         writer = csv.writer(csv_file, delimiter=",")
-        writer.writerow(liste_elements)
+        writer.writerow(elements_list)
 
     """
      2.c While loop that will go in the link of the current category and will check the presence of a next link
@@ -105,7 +110,7 @@ for category in (range(len(cleaned_links))):
     """
 
     while True:
-        page_request = requests.get(url_actuel)  # Scan of the page of the category represented by url_actual
+        page_request = requests.get(actual_url)  # Scan of the page of the category represented by url_actual
         soup = BeautifulSoup(page_request.content, 'html.parser')
         book_link_tags = soup.find_all("div", class_="image_container")  # Scraping of all links
         # of books on page 'n'
@@ -119,29 +124,28 @@ for category in (range(len(cleaned_links))):
             page_books.append("http://books.toscrape.com/catalogue/" + href_book)  # that we will join to url web page
             page_books = [e.replace('../', '') for e in page_books]
 
-            for books, item in enumerate(page_books):
+            for book, item in enumerate(page_books):
                 qty_scraped += 1
 
             """
             2.d For each link of the list "page_books", list of all the books of the page, we will scrape all the 
             information we want by entering one by one in each book links
             """
+            required_data = []
 
             for elements in range(len(page_books)):
                 page_request = requests.get(page_books[elements])
                 soup_book = BeautifulSoup(page_request.content, 'html.parser')
                 product_page_url = page_books[elements]
 
-
                 # Data is the scraped required data list
                 # at the end of each loop, it will be saved in the csv and then cleared
 
-                required_data = []
                 required_data.append(product_page_url)  # First element requested: url of the book
 
                 title = soup_book.find("h1")
-                ctitle = title.string
-                required_data.append(ctitle)  # Book title
+                title_text = title.string
+                required_data.append(title_text)  # Book title
 
                 table = soup_book.find("table", attrs={"class": "table table-striped"})
                 for i in table.find_all("td"):
@@ -154,17 +158,17 @@ for category in (range(len(cleaned_links))):
                            "four stars": "star-rating Four",
                            "five stars": "star-rating Five"}
 
-                rating = soup_book.find("div", class_= "col-sm-6 product_main")
+                rating = soup_book.find("div", class_="col-sm-6 product_main")
                 for value in reviews.values():
                     rating_par = rating.find("p", class_="{}".format(value))
                     if rating_par is not None:
-                        #print("{}".format(value))
+                        #  print("{}".format(value))
                         required_data.append("{}".format(value))
 
                 product_description = soup_book.find("p", class_=None)
                 if product_description is not None:
                     product_description = product_description.string
-                    required_data.append(product_description.text)
+                    required_data.append(product_description)
                 else:
                     product_description = "No description"
                     required_data.append(product_description)
@@ -180,22 +184,20 @@ for category in (range(len(cleaned_links))):
                 del required_data[5]
                 del required_data[6]
 
-
-
                 """
                 2.e Formatting of the book title to save and name the corresponding image in jpg 
                 + save in the folder of the category
                 """
 
-                to_remov = {",": "", "\;": "", "\.": "", "\:": "", "\!": "", "\?": "", "\)": "", "\(": "", " ": "_",
-                            "\*": "_", "\/": "_", "\-": "_", '"': '_', "'": "_", "__": "_"}
-                for char in to_remov.keys():
-                    ctitle = re.sub(char, to_remov[char], ctitle)
+                to_remove_title = {",": "", r"\;": "", r"\.": "", r"\:": "", r"\!": "", r"\?": "", r"\)": "", r"\(": "",
+                                   " ": "_", r"\*": "_", r"\/": "_", r"\-": "_", '"': '_', "'": "_", "__": "_"}
+                for char in to_remove_title.keys():
+                    title_text = re.sub(char, to_remove_title[char], title_text)
 
                 # If the title of the image > 150 characters, we shorten it to save it
-                ctitle = (ctitle[:150] + '..') if len(ctitle) > 150 else ctitle
+                title_text = (title_text[:150] + '..') if len(title_text) > 150 else title_text
 
-                image_save = "{}.jpg".format(ctitle)
+                image_save = "{}.jpg".format(title_text)
                 path_image = os.path.join(path, image_save)
                 webs = requests.get(src_image_root)
                 with open(path_image, 'wb') as f:
@@ -208,7 +210,7 @@ for category in (range(len(cleaned_links))):
                 with open(path_file, 'a', encoding="utf-8") as csv_file:
                     writer = csv.writer(csv_file)
                     writer.writerow(required_data)
-                    #print(required_data)
+                    # print(required_data)
                     required_data.clear()
                     page_books.clear()
 
@@ -220,4 +222,4 @@ for category in (range(len(cleaned_links))):
             print(str(remaining_qty) + " book(s) left to scrap. \n" + "Achievement = " + str(percentage_left) + " %")
             break
         # Otherwise, we join the current url by pasting the href to perform the pagination
-        url_actuel = urljoin(url_actuel, next_link["href"])
+        actual_url = urljoin(actual_url, next_link["href"])
